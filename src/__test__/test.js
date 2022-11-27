cab0b36e7e179a79e7dac5583e5fb367abbff04d
Maxim Slobozhanin <slob.max@gmail.com>
2022-11-27 15:19:29 +0400

    optimized code

--- src/__test__/test.js
+++ src/__test__/test.js
@@ -1,17 +1,21 @@
-import {
-  Character,
-  bowman,
-  swordsman,
-  magician,
-  bowmanTest,
-  swordsmanTest,
-  magicianTest,
-} from '../app';
+import Character from '../app';
 
-test('character', () => {
-  expect(bowman).toEqual(bowmanTest);
-  expect(swordsman).toEqual(swordsmanTest);
-  expect(magician).toEqual(magicianTest);
+test('character bowman undead', () => {
+  const bowman = new Character('Bowman', 'Bowman');
+  expect(bowman.attack).toBe(25);
+  expect(bowman.defence).toBe(25);
+});
+
+test('character wordsman zombie', () => {
+  const swordsman = new Character('Swordsman', 'Swordsman');
+  expect(swordsman.attack).toBe(40);
+  expect(swordsman.defence).toBe(10);
+});
+
+test('character magician daemon', () => {
+  const magician = new Character('Magician', 'Magician');
+  expect(magician.attack).toBe(10);
+  expect(magician.defence).toBe(40);
 });
 
 test('genError', () => {
--- src/app.js
+++ src/app.js
@@ -1,4 +1,4 @@
-export function Character(name, type) {
+export default function Character(name, type) {
   const arrOfType = ['Bowman', 'Swordsman', 'Magician', 'Daemon', 'Undead', 'Zombie'];
   if (name.length >= 2 && name.length <= 10 && arrOfType.includes(type)) {
     this.name = name;
@@ -21,34 +21,3 @@ export function Character(name, type) {
     throw new Error('Некорректные значения');
   }
 }
-
-export const bowman = new Character('Bowman', 'Bowman');
-export const swordsman = new Character('Swordsman', 'Swordsman');
-export const magician = new Character('Magician', 'Magician');
-
-export const bowmanTest = {
-  name: 'Bowman',
-  type: 'Bowman',
-  health: 100,
-  level: 1,
-  attack: 25,
-  defence: 25,
-};
-
-export const swordsmanTest = {
-  name: 'Swordsman',
-  type: 'Swordsman',
-  health: 100,
-  level: 1,
-  attack: 40,
-  defence: 10,
-};
-
-export const magicianTest = {
-  name: 'Magician',
-  type: 'Magician',
-  health: 100,
-  level: 1,
-  attack: 10,
-  defence: 40,
-};
