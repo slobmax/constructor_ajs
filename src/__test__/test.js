ce96a9e4b529e470ea0373b2f58713cc9cc56e04
Maxim Slobozhanin <slob.max@gmail.com>
2022-11-27 15:34:58 +0400

    task 2 completed

--- src/__test__/test.js
+++ src/__test__/test.js
@@ -2,20 +2,38 @@ import Character from '../app';
 
 test('character bowman undead', () => {
   const bowman = new Character('Bowman', 'Bowman');
-  expect(bowman.attack).toBe(25);
-  expect(bowman.defence).toBe(25);
+  expect(bowman).toEqual({
+    name: 'Bowman',
+    type: 'Bowman',
+    health: 100,
+    level: 1,
+    attack: 25,
+    defence: 25,
+  });
 });
 
 test('character wordsman zombie', () => {
   const swordsman = new Character('Swordsman', 'Swordsman');
-  expect(swordsman.attack).toBe(40);
-  expect(swordsman.defence).toBe(10);
+  expect(swordsman).toEqual({
+    name: 'Swordsman',
+    type: 'Swordsman',
+    health: 100,
+    level: 1,
+    attack: 40,
+    defence: 10,
+  });
 });
 
 test('character magician daemon', () => {
   const magician = new Character('Magician', 'Magician');
-  expect(magician.attack).toBe(10);
-  expect(magician.defence).toBe(40);
+  expect(magician).toEqual({
+    name: 'Magician',
+    type: 'Magician',
+    health: 100,
+    level: 1,
+    attack: 10,
+    defence: 40,
+  });
 });
 
 test('genError', () => {
